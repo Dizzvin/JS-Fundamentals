1f66f1fa0d1eb2c223ffb64d099eb2dd1997232f
Malcolm Odhiambo <vincentmalcom64@gmail.com>
2025-07-05 00:46:54 +0300

    Add 9-add.js script

---
+++ 9-add.js
@@ -0,0 +1,12 @@
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+function add(x, y) {
+  return x + y;
+}
+
+if (isNaN(a) || isNaN(b)) {
+  console.log("NaN");
+} else {
+  console.log(`Result: ${add(a, b)}`);
+}
